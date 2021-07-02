a9fab61aec84f05422707e3f7f6b834e349fda02
videjkk <viktarbarysievic@gmail.com>
2021-07-03 02:45:52 +0300

    cleaned react-app, index.js changes

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { useState, useEffect} from "react"
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App = () => {
+  const [task, setTask] = useState(null);
+  useEffect(() => {
+    const data = fetch("http://localhost:4000")
+      .then((response) => response.json())
+      .then((data) => setTask(data))
+      // .catch(err => );
+  }, [task]);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  if(!task){ return <div></div>; }
+  else{ return <div> tasks</div>}
+};
+
+
+ReactDOM.render(<App />, document.getElementById("root"));
