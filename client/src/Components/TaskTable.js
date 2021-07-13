c07c89e2d02b8f7cbb3eace0cd16219365dc204a
videjkk <viktarbarysievic@gmail.com>
2021-07-13 11:27:53 +0300

    some changes

--- client/src/Components/TaskTable.js
+++ client/src/Components/TaskTable.js
@@ -13,28 +13,29 @@ const TaskTable = (props) => {
    props.tasks.sort( (a,b) => a[sortBy] > b[sortBy] ? 1 : -1 ) 
    :
    props.tasks.sort( (a,b) => a[sortBy] > b[sortBy] ? -1 : 1 ) 
- },[sortOrder, props.tasks])
+ },
+ [sortOrder, props.tasks])
   
   return (
     <>
       <div className="tasktable header">
-        <div onClick={ () => sortTasks('title')} className="title">title</div>
+        <div onClick={ () => sortTasks('title')} className={`title `}>title</div>
         <div  onClick={ () => sortTasks('id')} className="id">id</div>
         <div  onClick={ () => sortTasks('priority')} className="priority">priority</div>
         <div  onClick={ () => sortTasks('status')} className="status">status</div>
       </div>
       <div className="tasktable">
-        {props.tasks.map( (task) => {
+        {props.tasks.map( (task, index) => {
           return (
             <>
-              <div className="title">{task.title}</div>
-              <div className="id">{task.id}</div>
-              <div className="priority">
+              <div key={task.title} className="title">{task.title}</div>
+              <div key={task.id} className="id">{task.id}</div>
+              <div key={task.priority} className="priority">
                 <span className={`span-priority ${task.priority}`}>
                   {task.priority}
                 </span>
               </div>
-              <div className="status">
+              <div key={task.status} className="status">
                 <span className={`span-status ${task.status}`}>
                   {task.status ? "done" : "on hold"}
                 </span>
