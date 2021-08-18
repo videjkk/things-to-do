acf33864fb6051649fc3db088a014ea1d2a7e6b4
videjkk <viktarbarysievic@gmail.com>
2021-08-19 01:47:47 +0300

    2

---
+++ webpack_react/webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'index.bundle.js'
+  },
+  devServer: {
+    port: 3000,
+    watchContentBase: true,
+    open: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /nodeModules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  plugins: [new HtmlWebpackPlugin({ template: './public/index.html' })],
+}
